7e7fc6dcc270d6b01ac18520205180654f326250
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-05 18:13:42 -0500

    Change to render front.html

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Handler(webapp2.RequestHandler):
 
 class MainHandler(Handler):
     def get(self):
-        self.write('asciichan')
+        self.render('front.html')
 
 
 app = webapp2.WSGIApplication([
