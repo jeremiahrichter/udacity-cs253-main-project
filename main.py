181039901b016076ac66bca200c8dd26e5b20495
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-08 06:38:49 -0500

    Add visit counting by cookie

--- main.py
+++ main.py
@@ -23,7 +23,12 @@ class Handler(webapp2.RequestHandler):
 
 class MainHandler(Handler):
     def get(self):
-        self.write('test!')
+        self.response.headers['Content-Type'] = 'text/plain'
+        visits = int(self.request.cookies.get('visits', 0))
+        visits += 1
+        self.response.headers.add_header('Set-Cookie', 'visits={}'.format(visits))
+
+        self.write("You've been here {} times".format(visits))
 
 
 app = webapp2.WSGIApplication([
