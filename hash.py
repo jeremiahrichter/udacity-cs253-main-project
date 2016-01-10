c2aa3cedf1a96cea0093f16122b1189e15740f80
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-10 09:11:07 -0500

    Add salt functions to hash.py

--- hash.py
+++ hash.py
@@ -1,6 +1,28 @@
+import hashlib
 import hmac
+import random
+import string
+
+ALPHA_NUM = string.letters + string.digits
+
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    return "{0},{1}".format(hashlib.sha256(name + pw + salt).hexdigest(), salt)
+
+
+def make_salt():
+    return "".join([str(random.choice(ALPHA_NUM)) for x in range(0, 5)])
+
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name, pw, salt)
+
 
 SECRET = 'ImASecret'
+
 
 def hash_str(s):
     return hmac.new(SECRET, s).hexdigest()
