848879a85468f13389a03675a5eea1146afe3ea2
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-10 15:58:59 -0500

    Add email to User model

--- models/user_model.py
+++ models/user_model.py
@@ -4,5 +4,6 @@ import header as h
 class User(h.db.Model):
     username = h.db.StringProperty(required=True)
     password = h.db.StringProperty(required=True)
+    email = h.db.StringProperty(required=True)
     created = h.db.DateTimeProperty(auto_now_add=True)
     last_modified = h.db.DateTimeProperty(auto_now=True)
