a408ad99e893cffd96b4f364e0881cadc51a8004
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-21 22:00:12 -0500

    Add get_coord() function

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -1,12 +1,37 @@
 from .handler_class import Handler
 import header as h
 from models.art_model import Art
+import urllib2 as u
+import json as j
+
+API_KEY = "e122a3095e9d27100d5a059fc091caee41eb32335ef248ebc98014b89eeab3dd"
+IP_URL = "http://api.ipinfodb.com/v3/ip-city/?key={0}&ip={1}&format=json"
 
 
 class MainHandler(Handler):
     def render_front(self, title='', art='', error=''):
         arts = h.db.GqlQuery('select * from Art order by created desc')
         self.render('front.html', title=title, art=art, error=error, arts=arts)
+
+    def get_coord(self, ip):
+        url = IP_URL.format(API_KEY, ip)
+        content = None
+        try:
+            content = u.urlopen(url).read()
+        except u.exceptions.HTTPError as e:
+            print(e.args)
+            return
+        if content:
+            try:
+                response = j.loads(content)
+                lat, lng = response['latitude'], response['longitude']
+                if lat != '0' and lng != '0':
+                    return h.db.GeoPt(lat, lng)
+                else:
+                    return
+            except j.JSONDecodeError as e:
+                print(e.args)
+                return
 
     def get(self):
         self.render_front()
