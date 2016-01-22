9dd0a2285609306ec00a7463e27d7503b8bc2d8a
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-21 22:04:18 -0500

    Add coords property

--- models/art_model.py
+++ models/art_model.py
@@ -4,3 +4,4 @@ class Art(h.db.Model):
     title = h.db.StringProperty(required=True)
     art = h.db.TextProperty(required=True)
     created = h.db.DateTimeProperty(auto_now_add=True)
+    coords = h.db.GeoPtProperty()
