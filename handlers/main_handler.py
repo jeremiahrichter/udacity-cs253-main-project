ce33a13aee0b3de230e6428d3fa1f8e95ad7a200
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-22 06:11:10 -0500

    Move get_coord() to header.py, add caching for points, art objects

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -1,37 +1,17 @@
 from .handler_class import Handler
 import header as h
 from models.art_model import Art
-import urllib2 as u
-import json as j
-
-API_KEY = "e122a3095e9d27100d5a059fc091caee41eb32335ef248ebc98014b89eeab3dd"
-IP_URL = "http://api.ipinfodb.com/v3/ip-city/?key={0}&ip={1}&format=json"
+from header import get_coord
 
 
 class MainHandler(Handler):
     def render_front(self, title='', art='', error=''):
         arts = h.db.GqlQuery('select * from Art order by created desc')
-        self.render('front.html', title=title, art=art, error=error, arts=arts)
 
-    def get_coord(self, ip):
-        url = IP_URL.format(API_KEY, ip)
-        content = None
-        try:
-            content = u.urlopen(url).read()
-        except u.exceptions.HTTPError as e:
-            print(e.args)
-            return
-        if content:
-            try:
-                response = j.loads(content)
-                lat, lng = response['latitude'], response['longitude']
-                if lat != '0' and lng != '0':
-                    return h.db.GeoPt(lat, lng)
-                else:
-                    return
-            except j.JSONDecodeError as e:
-                print(e.args)
-                return
+        arts = list(arts)
+        points = filter(None, (a.coords for a in arts))
+
+        self.render('front.html', title=title, art=art, error=error, arts=arts)
 
     def get(self):
         self.render_front()
@@ -41,7 +21,7 @@ class MainHandler(Handler):
         art = self.request.get('art')
         if title and art:
             a = Art(title=title, art=art, parent=h.art_key())
-            coords = self.get_coord(self.request.remote_addr)
+            coords = get_coord(self.request.remote_addr)
             if coords:
                 a.coords = coords
             a.put()
--- header.py
+++ header.py
@@ -2,6 +2,11 @@ import webapp2
 import os
 import jinja2
 from google.appengine.ext import db
+import urllib2 as u
+import json as j
+
+IP_API_KEY = "e122a3095e9d27100d5a059fc091caee41eb32335ef248ebc98014b89eeab3dd"
+IP_URL = "http://api.ipinfodb.com/v3/ip-city/?key={0}&ip={1}&format=json"
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -13,3 +18,24 @@ def blog_key(name='default'):
 
 def art_key(name='default'):
     return db.Key.from_path('art', name)
+
+
+def get_coord(ip):
+    url = IP_URL.format(IP_API_KEY, ip)
+    content = None
+    try:
+        content = u.urlopen(url).read()
+    except u.exceptions.HTTPError as e:
+        print(e.args)
+        return
+    if content:
+        try:
+            response = j.loads(content)
+            lat, lng = response['latitude'], response['longitude']
+            if lat != '0' and lng != '0':
+                return db.GeoPt(lat, lng)
+            else:
+                return
+        except j.JSONDecodeError as e:
+            print(e.args)
+            return
