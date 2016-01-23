1acbb52c747e5b9d5b6cbed2d9871a9e79a6e728
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-23 16:09:14 -0500

    Add memcache support to post permalinks

--- handlers/post_handler.py
+++ handlers/post_handler.py
@@ -1,13 +1,14 @@
 import header as h
 from header import blog_key
 from .handler_class import Handler
-
+from .post_header import get_post
+import time
 
 class PostHandler(Handler):
     def get(self, post_id):
-        key = h.db.Key.from_path('Post', int(post_id), parent=blog_key())
-        post = h.db.get(key)
+        post, last_updated = get_post(post_id)
         if post:
-            self.render('solo_post.html', post=post)
+            self.render('solo_post.html', post=post,
+                        last_updated=(time.time() - last_updated))
         else:
             self.error(404)
--- handlers/post_header.py
+++ handlers/post_header.py
@@ -1,6 +1,8 @@
 from google.appengine.api import memcache
 from models.post_model import Post
 import time
+import header as h
+from header import blog_key
 
 
 def top_ten(update=False):
@@ -10,3 +12,12 @@ def top_ten(update=False):
         posts = (list(Post.all().order('-created').run(limit=10)), time.time())
         memcache.set(key, posts)
     return posts
+
+
+def get_post(post_id):
+    post = memcache.get(post_id)
+    if not post:
+        key = h.db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = h.db.get(key), time.time()
+        post and memcache.set(post_id, post)
+    return post
