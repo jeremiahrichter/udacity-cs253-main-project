63f7d86948bdc4ad8bd8bfa21ed2f3723064812d
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-25 06:06:42 -0500

    Change to functions from previous homework solution for bonus question

--- header.py
+++ header.py
@@ -8,7 +8,3 @@ jinja2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), au
 
 def users_key(group='default'):
     return db.Key.from_path('users', group)
-
-
-def page_key(group='default'):
-    return db.Key.from_path('page', group)
--- models/page_model.py
+++ models/page_model.py
@@ -1,5 +1,4 @@
 import header as h
-from header import page_key
 from handlers.wiki_page_header import markdown_to_html, escape_html
 
 
@@ -8,6 +7,19 @@ class Page(h.db.Model):
     content = h.db.TextProperty()
     created = h.db.DateTimeProperty(auto_now_add=True)
     last_modified = h.db.DateTimeProperty(auto_now=True)
+
+    @staticmethod
+    def parent_key(path):
+        return h.db.Key.from_path('/root' + path, 'page')
+
+    @classmethod
+    def by_id(cls, page_id, path):
+        return cls.get_by_id(page_id, cls.parent_key(path))
+
+    @classmethod
+    def all_by_path(cls, path):
+        q = cls.all().ancestor(cls.parent_key(path)).order('-created')
+        return q
 
     @classmethod
     def by_url(cls, url):
@@ -18,7 +30,7 @@ class Page(h.db.Model):
     def find_page(cls, url):
         p = Page.by_url(url)
         if not p:
-            p = Page(url=url, content=h.db.Text(u''), parent=page_key())
+            p = Page(url=url, content=h.db.Text(u''), parent=Page.parent_key(url))
         return p
 
     def update_content(self, content):
