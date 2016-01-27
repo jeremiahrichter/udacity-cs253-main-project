72b45ea6a250f5679a413c2b8ea4a13dd4c9853b
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-01-27 17:57:54 -0500

    Add basic history page and fix navigation bugs in the navbar

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from handlers.login_handler import LoginHandler
 from handlers.logout_handler import LogoutHandler
 from handlers.wiki_page_handler import WikiPageHandler
 from handlers.edit_page_handler import EditPageHandler
-
+from handlers.history_page_handler import HistoryPageHandler
 PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 app = webapp2.WSGIApplication([
     ('/signup', SignupHandler),
     ('/login', LoginHandler),
     ('/logout', LogoutHandler),
     ('/_edit' + PAGE_RE, EditPageHandler),
+    ('/_history' + PAGE_RE, HistoryPageHandler),
     (PAGE_RE, WikiPageHandler)
 ], debug=True)
